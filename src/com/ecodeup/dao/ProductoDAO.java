4d00f6b9e5952fc93f718f0dbbc37d1787c30c3f
afelro97 <afelro97@gmail.com>
2020-05-20 14:25:20 -0500

    Crud

--- src/com/ecodeup/dao/ProductoDAO.java
+++ src/com/ecodeup/dao/ProductoDAO.java
@@ -14,10 +14,36 @@ public class ProductoDAO {
 	private boolean estadoOperacion;
 	
 	//guardar
-	public boolean guardar(Producto producto) {
+	public boolean guardar(Producto producto) throws SQLException {
 		String sql=null;
 		estadoOperacion=false;
-		return true;
+		connection=obtenerConexion();
+		
+		try {
+			connection.setAutoCommit(false);
+			// campos de la base de datos donde se inserta, MODIFICAR
+			sql="INSERT INTO productos(id, nombre, cantidad, precio, fecha_crear, fecha_actualizar) VALUES(?,?,?,?,?,?)"; //los signos de interrogación son la cantidad de campos de la tabla a modificar
+			statement=connection.prepareStatement(sql);
+			
+			statement.setString(1, null);
+			statement.setString(2, producto.getNombre()); //obtener valor del objeto producto
+			statement.setDouble(3, producto.getCantidad());
+			statement.setDouble(4, producto.getPrecio());
+			statement.setDate(5, producto.getFechaCrear());
+			statement.setDate(6, producto.getFechaActualizar());
+			
+			estadoOperacion = statement.executeUpdate()>0;
+			
+			connection.commit();
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			connection.rollback();
+			e.printStackTrace();
+		}
+		
+		
+		return estadoOperacion;
 	}
 	
 	//editar
